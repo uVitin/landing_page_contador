80d55d97c69baa13fa9323dda06fdc884fd202c4
Albert Vitor <albertvitor18@outlook.com>
2024-07-29 13:17:44 -0300

    Landing Page - Contador!

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+const dataDoEvento = new Date("Mar 26, 2025  20:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distaciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distaciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distaciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distaciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distaciaAteOEvento % minutoEmMs) / 1000)
+
+    document.getElementById('contador').innerHTML = `O evento se encerra em ${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distaciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `O evento acabou!`
+    }
+}, 1000)
